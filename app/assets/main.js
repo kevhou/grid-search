3f41eb884bde3705f899032773d659ac36981c5a
kevhou <kevhou1995@gmail.com>
2015-05-25 16:30:36 -0400

    add navigation to /r/sub to view specific sub imgur

--- app/assets/main.js
+++ app/assets/main.js
@@ -1,59 +1,48 @@
-// angular.module('grid', ['ngMaterial'])
-// .controller('ctrl', function($scope){
-//   $scope.pictures = [];
+var app = angular.module('myapp', ['ngLoadingSpinner', 'ngMaterial', 'ngRoute']);
 
-//   $scope.init = function(){
-//     var url = 'https://api.imgur.com/3/gallery/r/';
-//     var sub = 'ferrari.json';
-//     var final = url + sub;
-
-//     $.ajax({
-//       url: final,
-//       dataType: 'json',
-//       beforeSend: function (xhr) {
-//           xhr.setRequestHeader('Authorization', 'Client-ID 6ec1b25178e106f');
-//       },
-//       success: function(imgur){
-//         $scope.pictures = imgur.data;
-//       }
-//     })
-//   }
-  
-// })
-// .directive('backImg', function(){
-//   return function(scope, element, attrs){
-//       var url = attrs.backImg;
-//       var position = url.length-4;
-//       var output = [url.slice(0, position), 'm', url.slice(position)].join('');
-
-//       element.css({
-//         'background-image': 'url(' + url + ')',
-//         'background-size': 'cover'
-//       });
-//   };
-// });
-
-var app = angular.module('myapp', ['ngLoadingSpinner', 'ngMaterial']);
-app.controller('MyController', function($scope, $http) {
+app.controller('MyController', function($scope, $http, $routeParams) {
+  var search;
+  if(typeof $routeParams.search == 'undefined'){
+    search = 'ferrari';
+  }else{
+    search = $routeParams.search;
+  }
   $scope.startAjax = function() {
     $http.defaults.headers.get = {'Authorization': 'Client-ID 6ec1b25178e106f'}
-    $http.get('https://api.imgur.com/3/gallery/r/dog.json')
+    $http.get('https://api.imgur.com/3/gallery/r/' + search + '.json')
     .success(function(imgur){
       $scope.pictures = imgur.data;
     });
   };
 });
+
 app.directive('backImg', function(){
   return {
     link: function(scope, element, attrs){
-      var url = attrs.backImg;
-      var position = url.length-4;
-      var output = [url.slice(0, position), 'm', url.slice(position)].join('');
-
+      var link = attrs.backImg;
+      var extensionIndex = link.lastIndexOf('.');
+      var extension = link.substr(extensionIndex);
+      if(extension === '.png' || extension === '.jpg'){
+        var url = link.substring(0, extensionIndex) + "b" + extension;
+      }else{
+        var url = link; 
+      }
       element.css({
         'background-image': 'url(' + url + ')',
         'background-size': 'cover'
       });
     }
   };
+});
+
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: '/partials/grid.html'
+    })
+    .when('/r/:search', {
+      templateUrl: '/partials/grid.html',
+    })
+
+    $locationProvider.html5Mode(true);
 });
